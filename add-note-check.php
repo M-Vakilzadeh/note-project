33db16b2697787203a269e1dda8a7978032c6ca2
Mohsen vakilzadeh <mohsenvakilzadeh@gmail.com>
2021-08-21 22:04:11 +0430

    last

--- add-note-check.php
+++ add-note-check.php
@@ -5,7 +5,7 @@ $title = $_POST['title'];
 $description = $_POST['description'];
 $dateTime = $_POST['dateTime'];
 
-
+echo $dateTime;
 
 if (!isset($_SESSION['user_id'])){
     header("location: home.php");
@@ -13,12 +13,14 @@ if (!isset($_SESSION['user_id'])){
 
 $userId = $_SESSION['user_id'];
 
+
+die;
 $db = Db::getInstance();
 
 $db->insert("INSERT INTO x_note
 (title, description, user_id, isDone, eventTime) VALUES 
 ('$title', '$description', '$userId', '0', '$dateTime')");
 
-header("location: home.php");
+//header("location: home.php");
  
 ?>
