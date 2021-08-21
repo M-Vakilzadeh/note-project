a857f05c8cefebf31c28dffef70be2f723c21968
Mohsen vakilzadeh <mohsenvakilzadeh@gmail.com>
2021-08-21 10:21:23 +0430

    21.8.co

--- index.php
+++ index.php
@@ -5,10 +5,11 @@
 <body>
     
     <?php
-    require_once('main.php');
-    $db = Db::getInstance();
-    $records = $db->query("SELECT * FROM x_note");
-    dump($records);
+    //require_once('main.php');
+    //$db = Db::getInstance();
+    //$records = $db->query("SELECT * FROM x_note");
+    //dump($records);
+  require_once('home.php')
   ?>
 </body>
 </html>
